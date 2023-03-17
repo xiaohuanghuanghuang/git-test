5d06e85dd79acebf7326378de56b4c31152cee4b
xiaohuanghuanghuang <128049268+xiaohuanghuanghuang@users.noreply.github.com>
2023-03-17 15:23:44 +0800

    github commit

--- src/main/java/com/atdaxue/git/GitTest.java
+++ src/main/java/com/atdaxue/git/GitTest.java
@@ -15,6 +15,7 @@ public class GitTest {
         System.out.println("hotfix test");
         System.out.println("push test");
         System.out.println("pull test");
+        System.out.println("github test");
 
     }
 }
